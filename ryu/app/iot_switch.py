fa1e905858256a066ad8624e306e1b66c810841e
Pavel Abramov <uncle.decart@gmail.com>
2020-05-30 11:25:17 +0300

    Added dijsktra

--- ryu/app/iot_switch.py
+++ ryu/app/iot_switch.py
@@ -7,7 +7,9 @@ from ryu.lib.mac import haddr_to_bin
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
+from collections import defaultdict
 
+from ryu.lib.dpid import dpid_to_str
 import os
 
 from webob.static import DirectoryApp
@@ -23,6 +25,51 @@ import datetime
 import random 
 
 PATH = os.path.dirname(__file__)
+
+class Graph:
+  def __init__(self):
+    self.nodes = set()
+    self.edges = defaultdict(list)
+    self.distances = {}
+
+  def add_node(self, value):
+    self.nodes.add(value)
+
+  def add_edge(self, from_node, to_node, distance):
+    self.edges[from_node].append(to_node)
+    self.edges[to_node].append(from_node)
+    self.distances[(from_node, to_node)] = distance
+
+
+def dijsktra(graph, initial):
+  visited = {initial: 0}
+  path = {}
+
+  nodes = set(graph.nodes)
+
+  while nodes: 
+    min_node = None
+    for node in nodes:
+      if node in visited:
+        if min_node is None:
+          min_node = node
+        elif visited[node] < visited[min_node]:
+          min_node = node
+
+    if min_node is None:
+      break
+
+    nodes.remove(min_node)
+    current_weight = visited[min_node]
+
+    for edge in graph.edges[min_node]:
+      weight = current_weight + graph.distance[(min_node, edge)]
+      if edge not in visited or weight < visited[edge]:
+        visited[edge] = weight
+        path[edge] = min_node
+
+  return visited, path
+
 
 
 class SimpleSwitch(app_manager.RyuApp):
@@ -61,6 +108,16 @@ class SimpleSwitch(app_manager.RyuApp):
         flags=ofproto.OFPFF_SEND_FLOW_REM, actions=actions)
       datapath.send_msg(mod)
 
+    def dpid_state_from_mininet(self, name):
+      if 'h' in name:
+        return (0,0,0)
+      dpid = dpid_to_str(int(name[1:]))
+      query = "SELECT * FROM charge_state WHERE dpid = %s" % dpid 
+      self.cursor.execute(query)
+      rv = self.cursor.fetchall() 
+      print "OLOLOLO ", rv
+      return rv[0]
+
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
       msg = ev.msg
@@ -81,13 +138,53 @@ class SimpleSwitch(app_manager.RyuApp):
 
       self.logger.info("packet in %s %s %s %s", dpid, src, dst, msg.in_port)
 
-      # learn a mac address to avoid FLOOD next time.
-      self.mac_to_port[dpid][src] = msg.in_port
+      self.cursor.execute('SELECT * FROM topology ORDER BY id;')
+      links = self.cursor.fetchall()
 
-      if dst in self.mac_to_port[dpid]:
-        out_port = self.mac_to_port[dpid][dst]
-      else:
-        out_port = ofproto.OFPP_FLOOD
+      query = "SELECT * FROM mac_to_dpid"
+      self.cursor.execute(query)
+      rv = self.cursor.fetchall() 
+      print rv
+      if len(rv) == 0:
+        return
+      nodes = set()
+      g = Graph()
+      for pos, node_a, node_b in links:
+        nodes.add(node_a)
+        nodes.add(node_b)
+        tempo_testo = self.dpid_state_from_mininet(node_a)
+        _, node_a_charge, _  = self.dpid_state_from_mininet(node_a)
+        _, node_b_charge, _  = self.dpid_state_from_mininet(node_b)
+        g.add_edge(node_a, node_b, 1/node_a_charge if node_a_charge != 0 else 0)
+        g.add_edge(node_b, node_a, 1/node_b_charge if node_b_charge != 0 else 0)
+
+      for node in nodes:
+        g.add_node(node)
+
+      cur_node = 's%d' % dpid
+      _, path = dijsktra(g, cur_node)
+      dst_node = self.cursor.execute('SELECT dpid FROM mac_to_dpid WHERE mac_addr = %s' % eth.dst)
+      if 'h' not in dst_node:
+        dst_node = 's%d' % int(dst_node)
+
+      while path[dst_node] != cur_node:
+        dst_node = path[dst_node]
+
+      result_port = 0
+      for _, node_a, node_b in links:
+        if cur_node in [node_a, node_b]:
+          result_port += 1
+          if dst_node in [node_a, node_b]:
+            break
+      out_port = result_port
+
+      # learn a mac address to avoid FLOOD next time.
+      #self.mac_to_port[dpid][src] = msg.in_port
+
+      #if dst in self.mac_to_port[dpid]:
+      #  out_port = self.mac_to_port[dpid][dst]
+      #else:
+      #  out_port = ofproto.OFPP_FLOOD
 
       query = "SELECT * FROM charge_state WHERE dpid = %s" % dpid 
       self.cursor.execute(query)
